e99eeb20837c145ef5c2e15c8ae49d554afcf2e4
pavansai <pavansaivvit@gmail.com>
2023-05-23 07:01:57 +0000

    post error resolved and till API 4 the code is completed

--- app.js
+++ app.js
@@ -46,10 +46,25 @@ app.get("/players/", async (request, response) => {
 //POST API
 app.post("/players/", async (request, response) => {
   const { playerName, jerseyNumber, role } = request.body;
-  const createPlayerSqlQuery = `INSERT INTO cricket_team (player_name, jersey_number, role) VALUES (${"playerName"}, ${jerseyNumber}, ${"role"});`;
+  const createPlayerSqlQuery = `INSERT INTO cricket_team (player_name, jersey_number, role) VALUES ("${playerName}", ${jerseyNumber}, "${role}");`;
 
   await db.run(createPlayerSqlQuery);
   response.send("Player Added to Team");
+});
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayerQuarry = `SELECT * FROM cricket_team WHERE player_id = ${playerId};`;
+  const playerDetails = await db.get(getPlayerQuarry);
+  response.send(playerDetails);
+});
+
+app.put("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const { playerName, jerseyNumber, role } = request.body;
+  const playerDetailsUpdateQuarry = `UPDATE cricket_team SET player_name = "${playerName}", jersey_number = ${jerseyNumber}, role = "${role}" WHERE player_id = ${playerId};`;
+  await db.run(playerDetailsUpdateQuarry);
+  response.send("Player Details Updated");
 });
 
 module.exports = app;
